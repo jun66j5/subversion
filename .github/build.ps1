8501a5ede70ca32cf32ab4a6179c582620d7eb2c
Jun Omae <jun66j5@gmail.com>
2021-05-26 23:22:52 +0900

    Split the job for Windows to core and bindings

--- .github/build.ps1
+++ .github/build.ps1
@@ -1,27 +1,45 @@
-$LocalAppData = $env:LocalAppData
-$ProgramData = $env:ProgramData
-$workspace = $env:GITHUB_WORKSPACE
-$arch = $env:MATRIX_ARCH
-$pythonLocation = $env:pythonLocation
+$LocalAppData = $Env:LocalAppData
+$ProgramData = $Env:ProgramData
+$workspace = $Env:GITHUB_WORKSPACE
+$arch = $Env:MATRIX_ARCH
+$pythonLocation = $Env:pythonLocation
 $vcpkg_root = 'C:\vcpkg'
 $vcpkg_triplet = "$arch-windows"
 $vcpkg_dir = "$vcpkg_root\installed\$vcpkg_triplet"
 $sqlite_name = 'sqlite-amalgamation-3081101'
 $sqlite_url = "https://www.sqlite.org/2015/$sqlite_name.zip"
 $sqlite_arc = "$workspace\$sqlite_name.zip"
-$java_home = $env:JAVA_HOME
+$java_home = $Env:JAVA_HOME
 $junit_url = 'https://search.maven.org/remotecontent?filepath=junit/junit/4.13.2/junit-4.13.2.jar'
 $junit_file = "$workspace\junit4.jar"
-$python = "$pythonLocation\python.exe"
+$swig3_name = 'swigwin-3.0.12'
+$swig3_url = "https://sourceforge.net/projects/swig/files/swigwin/$swig3_name/$swig3_name.zip/download"
+$swig3_arc = "$workspace\$swig3_name.zip"
+$python = ($pythonLocation -eq $null) ? 'python.exe' : "$pythonLocation\python.exe"
 
-& choco install -y --no-progress swig
 & vcpkg install "--triplet=$vcpkg_triplet" apr apr-util zlib gettext
 Invoke-WebRequest -Uri $sqlite_url -OutFile $sqlite_arc
-Expand-Archive -LiteralPath $sqlite_arc -DestinationPath "$workspace"
-Invoke-WebRequest -Uri $junit_url -OutFile $junit_file
+Expand-Archive -LiteralPath $sqlite_arc -DestinationPath $workspace
 
-New-Item -Force -ItemType Directory `
-         -Path "subversion\bindings\swig\proxy"
+switch -Exact ($args[0]) {
+    'all' {
+        $genmake_opts = @()
+        $build_targets = '__MORE__'
+        $test_targets = @('--parallel')
+    }
+    'bindings' {
+        $genmake_opts = @("--with-swig=$ProgramData\chocolatey\bin",
+                          "--with-py3c=$workspace\py3c",
+                          "--with-jdk=$java_home",
+                          "--with-junit=$junit_file")
+        $build_targets = '__ALL__;__SWIG_PYTHON__;__SWIG_PERL__;__JAVAHL__;__JAVAHL_TESTS__'
+        $test_targets = @('--swig=python', '--swig=perl', '--javahl')
+        & choco install -y --no-progress swig
+        Invoke-WebRequest -Uri $junit_url -OutFile $junit_file
+    }
+}
+
+New-Item -Force -ItemType Directory -Path "subversion\bindings\swig\proxy"
 $rc = 0
 & $python gen-make.py `
           --vsnet-version=2019 `
@@ -31,25 +49,18 @@ $rc = 0
           "--with-zlib=$vcpkg_dir" `
           "--with-libintl=$vcpkg_dir" `
           "--with-sqlite=$workspace\$sqlite_name" `
-          "--with-swig=$ProgramData\chocolatey\bin" `
-          "--with-py3c=$workspace\py3c" `
-          "--with-jdk=$java_home" `
-          "--with-junit=$junit_file"
+          $genmake_opts
 & msbuild subversion_vcnet.sln `
           /nologo /v:q /m:3 /fl /flp:logfile=msbuild.log `
-          "/t:__MORE__;__SWIG_PYTHON__;__SWIG_PERL__;__JAVAHL__;__JAVAHL_TESTS__" `
+          "/t:$build_targets" `
           "/p:Configuration=Release;Platform=$arch"
 if (-not $?) {
     Write-Warning "Exited with $LASTEXITCODE"
     $rc = 1
 }
-$env:PATH = "$vcpkg_dir\bin;$($env:PATH)"
-foreach ($args in @('--parallel',
-                    '--swig=python',
-                    '--swig=perl',
-                    '--javahl'))
-{
-    & $python win-tests.py -cr $args
+$Env:PATH = "$vcpkg_dir\bin;$($Env:PATH)"
+foreach ($test_args in $test_targets) {
+    & $python win-tests.py -cr $test_args
     if (-not $?) {
         Write-Warning "Exited with $LASTEXITCODE"
         $rc = 1
